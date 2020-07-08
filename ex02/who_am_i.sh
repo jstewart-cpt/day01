956ff4039aea5cc6f728fa05fd83283b7922455f
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 10:03:32 +0200

    Updated who_am_i.sh (shell script works)

--- ex02/who_am_i.sh
+++ ex02/who_am_i.sh
@@ -1,3 +1,4 @@
-$>sh who_am_i.sh
-dn:uid=jstewart,cn=gssapi,cn=auth
-%>
+#Save the whoami command in the search variable
+search=`whoami`
+#perform an ldap search (use -x to use simple auth instead of SASL), use grep to narrow down the return results to dn, then cut the dn: name key out of the results. This should return only the value of the designated name.
+ldapsearch -x "(uid=$search)" dn | grep '^dn:' | cut -c5-999
