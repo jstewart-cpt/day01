b65bd2b9c0c76e4513da4278c37eed6e33faf01c
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 15:07:20 +0200

    Update print_groups.sh

--- ex04/print_groups.sh
+++ ex04/print_groups.sh
@@ -1,4 +1,4 @@
-#Display the groups for the FT_USER env, then use the tr or transliterate command to replace any spaces with commas.
+#Display the groups for the FT_USER env, then use the 'tr' command to replace any spaces with commas.
 groups $FT_USER | tr ' ' ','
 
 #The output should display your groups list.
