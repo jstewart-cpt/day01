347a78ef8c2d8d43446486babd9f4a09b7f9cec7
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 14:27:36 +0200

    Update count_files.sh

--- ex08/count_files.sh
+++ ex08/count_files.sh
@@ -1,3 +1,3 @@
-#Find 'PWD'diaplays the directory, therefore by indicating the addition commands, the shell searhces through all the directory, a well as sub directories, and incdicating the no. of files.
+#Find 'PWD'displays the directory, therefore by indicating the addition commands, the shell searhces through all the directory, a well as sub directories, and incdicating the no. of files.
 
 find $PWD \( -type f -or -type d \) -name ".*" -o -print | wc -l
