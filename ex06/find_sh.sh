68497ae1a5998755d4ac1860a60a3823c80e2cf8
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 15:10:08 +0200

    Update find_sh.sh

--- ex06/find_sh.sh
+++ ex06/find_sh.sh
@@ -1,3 +1,3 @@
-#'ls -R' command enables a loop to search each files directory and all of its sub directories , therefore the pipelined 'grep \.sh$' command specifies the type of files being searched. 'cut -f' cuts the display of the extension type from the output display.
+#'ls -R' command starts a search through all the directories and all of its sub directories , therefore the pipelined 'grep \.sh$' command specifies the type of files being searched for that you want displayed. 'cut -f' cuts the display of the extension type from the output display.
 
 ls -R | grep "\.sh$" | cut -f 1 -d '.'
