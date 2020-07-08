823aa1143b585e5a116c9433d1025ddf6f66ed57
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 14:26:48 +0200

    Update count_files.sh

--- ex08/count_files.sh
+++ ex08/count_files.sh
@@ -1,3 +1,3 @@
-#Using 'ls' lists the files, by add '-R',you enable it to search the  
+#Find 'PWD'diaplays the directory, therefore by indicating the addition commands, the shell searhces through all the directory, a well as sub directories, and incdicating the no. of files.
 
 find $PWD \( -type f -or -type d \) -name ".*" -o -print | wc -l
