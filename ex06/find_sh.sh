53d48f890fb0c4cdf8d17a2e2770336c74c869dc
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 11:03:46 +0200

    Comments added to proved more clarity
    still looking for a way display them without the '.sh'

--- ex06/find_sh.sh
+++ ex06/find_sh.sh
@@ -1,1 +1,3 @@
+#'ls -R' command enables a loop to search each files directory and all of its sub directories , therefore the pipelined 'grep' command specifies the type of files being searched.
+
 ls -R |grep "\.sh$"
