7392eaf3da4d0cd9803919056190985f22ad15fe
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-08 15:57:03 +0200

    Update skip.sh

--- day01/ex14/skip.sh
+++ day01/ex14/skip.sh
@@ -1,3 +1,4 @@
-#" awk 'NR % 2 == 1'" This displays every second line starting with the first one. Therefore by using the 'ls -1' command I can displays every other line.
+#"awk" is used to scan each input file and identify any patterns within the file or a program. 
+#"awk 'NR % 2 == 1'";This displays every second line starting with the first one. Therefore by using the 'ls -1' command I can display every other line.
 
 ls -1 | awk 'NR % 2 == 1'
