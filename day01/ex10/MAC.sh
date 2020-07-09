c24b894be5918dabbf5bd9af4e325eead88f8a09
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-09 13:00:57 +0200

    Updated

--- day01/ex10/MAC.sh
+++ day01/ex10/MAC.sh
@@ -1,3 +1,5 @@
-#'ifconfig' displays the MAC's complete network information, then pipelined 'grep' to filter only the information on 'ether', then I 'cut' the other information out so that only the MAC's address shows.
+#'ifconfig' displays the MAC's complete network information.
+#Then pipelined 'grep' to filter only the information on 'ether'.
+#Then I 'cut' the other information out so that only the MAC's address shows.
 
 ifconfig | grep "ether" | cut -c 8-
